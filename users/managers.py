02eab0615a809c282f344595cc1c19d402e2066a
Abdulkhafiz <auz.offical@gmail.com>
2023-09-16 09:58:00 +0500

    .

---
+++ users/managers.py
@@ -0,0 +1,30 @@
+
+from django.contrib.auth.models import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email address must be provided.')
+        if not user_id:
+            raise ValueError(_('The user_id field must be set'))
+        if not username:
+            raise ValueError(_('The username field must be set'))
+        if not first_name:
+            raise ValueError(_('The First Name field must be set'))
+        if not last_name:
+            raise ValueError(_('The lastname field must be set'))
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, user_id=user_id, first_name=first_name, last_name=last_name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+
+        return self.create_user(username, user_id, first_name, last_name, email, password, **extra_fields)
--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-from .managers import CustomUserManager
+from users.managers import CustomUserManager
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db.models.signals import pre_save
@@ -12,7 +12,7 @@ import random
 def generate_unique_user_id():
     while True:
         user_id = str(random.randint(10001, 99999))
-        if not CustomUser.objects.filter(user_id=user_id).exists():
+        if not Users.objects.filter(user_id=user_id).exists():
             break
     return user_id
 
@@ -22,9 +22,37 @@ def generate_user_id(sender, instance, **kwargs):
         instance.user_id = generate_unique_user_id()
 
 class Users(AbstractBaseUser, PermissionsMixin):
-    user_id = models.IntegerField(_('User ID'), max_length=6, blank=True, null=True, unique=True, default=generate_unique_user_id)
+    user_id = models.IntegerField(_('User ID'), max_length=6, blank=False, null=False, unique=True, default=generate_unique_user_id)
+    username = models.CharField(_('Username'), max_length=15, blank=False, null=False, unique=True)
     email = models.EmailField(_('Email'), unique=True, blank=True, null=True)
-    
+    first_name = models.CharField(_('Ism'), max_length=50, blank=False, null=False)
+    last_name = models.CharField(_('Familya'), max_length=50, blank=False, null=False)
+    image = models.ImageField(default='', upload_to='Users/%Y/%m/')
+    mobile = models.IntegerField(max_length=20, blank=True, null=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    last_login = models.DateTimeField(default=timezone.now)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        if not self.user_id:
+            self.user_id = generate_unique_user_id()
+        super().save(*args, **kwargs)
+
+    REQUIRED_FIELDS = ['user_id', 'username', 'first_name', 'last_name']
+    USERNAME_FIELD = 'username'
+    objects = CustomUserManager()
+
+    def full_name(self):
+        full_name = self.first_name + ' ' + self.last_name
+        return full_name
+
+    class Meta:
+        verbose_name = 'Foydalanuvchi'
+        verbose_name_plural = 'Foydalanuvchilar'
+
+    def __str__(self):
+        return str(self.full_name)
 
 
 
