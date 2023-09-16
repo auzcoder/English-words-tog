c9a0dddfe806bcfdafc18c77f566b05f6d0d20ef
Abdulkhafiz <auz.offical@gmail.com>
2023-09-16 09:09:12 +0500

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
+from .managers import CustomUserManager
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+import random
 
-# Create your models here.
+
+
+def generate_unique_user_id():
+    while True:
+        user_id = str(random.randint(10001, 99999))
+        if not CustomUser.objects.filter(user_id=user_id).exists():
+            break
+    return user_id
+
+@receiver(pre_save, sender=CustomUserManager)
+def generate_user_id(sender, instance, **kwargs):
+    if not instance.user_id:
+        instance.user_id = generate_unique_user_id()
+
+
+        
